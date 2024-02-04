c6c798f00443e99fe156293bab461f973cb9a3a5
Rylarso <rylarso@gmail.com>
2024-02-03 17:08:36 -0700

    server working

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello this is a test uwu");
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+    res.send("Hellow");
+});
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("hello this is a test uwu");
+import express, { Express, Request, Response } from 'express';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hellow");
+});
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
